558ef8c550e0321199d2b81a249f2d73eabe2a24
Mustapha Amin <mustaphaamin2003@gmail.com>
2025-10-24 08:22:39 +0100

    refactor: fixed medical center layout

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -21,67 +21,72 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SearchArea(),
-            SizedBox(height: 25),
-            SizedBox(
-              height: 200,
-              child: PageView.builder(
-                itemCount: 3,
-                onPageChanged: (index) {
-                  setState(() {
-                    currentPage = index;
-                  });
-                },
-                itemBuilder: (context, index) {
-                  return AppointmentCard();
-                },
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  SearchArea(),
+                  SizedBox(height: 25),
+                  SizedBox(
+                    height: 200,
+                    child: PageView.builder(
+                      itemCount: 3,
+                      onPageChanged: (index) {
+                        setState(() {
+                          currentPage = index;
+                        });
+                      },
+                      itemBuilder: (context, index) {
+                        return AppointmentCard();
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: 80,
+                    child: StepProgressIndicator(
+                      selectedColor: Color(0xff0099FF),
+                      totalSteps: 3,
+                      unselectedColor: Colors.grey[200]!,
+                      currentStep: currentPage + 1,
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    spacing: 15,
+                    children: [
+                      Expanded(
+                        child: AppointmentQuickActionCard(
+                          icon: SVGAssets.docAppt,
+                          title: "Book Doctor Apppointment",
+                          subtitle: "Find a Doctor or Specialist",
+                          color: Color(0xffF9F5FF),
+                          iconBgColor: Color(0xffE7EFFF),
+                          borderColor: Color(0xffA0B6EA),
+                        ),
+                      ),
+                      Expanded(
+                        child: AppointmentQuickActionCard(
+                          icon: SVGAssets.hospAppt,
+                          title: "Book Hospital Appointment",
+                          subtitle: "Locate nearby hospital to visit",
+                          color: Color(0xffEDFCF2),
+                          iconBgColor: Color(0xffD3F8DF),
+                          borderColor: Color(0xffAAF0C4),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  MedicalCategories(),
+                  SizedBox(height: 12),
+                  DoctorsSection(),
+                ],
               ),
             ),
-            SizedBox(height: 10),
-            SizedBox(
-              width: 80,
-              child: StepProgressIndicator(
-                selectedColor: Color(0xff0099FF),
-                totalSteps: 3,
-                unselectedColor: Colors.grey[200]!,
-                currentStep: currentPage + 1,
-              ),
-            ),
-            SizedBox(height: 25),
-            Row(
-              spacing: 15,
-              children: [
-                Expanded(
-                  child: AppointmentQuickActionCard(
-                    icon: SVGAssets.docAppt,
-                    title: "Book Doctor Apppointment",
-                    subtitle: "Find a Doctor or Specialist",
-                    color: Color(0xffF9F5FF),
-                    iconBgColor: Color(0xffE7EFFF),
-                    borderColor: Color(0xffA0B6EA),
-                  ),
-                ),
-                Expanded(
-                  child: AppointmentQuickActionCard(
-                    icon: SVGAssets.hospAppt,
-                    title: "Book Hospital Appointment",
-                    subtitle: "Locate nearby hospital to visit",
-                    color: Color(0xffEDFCF2),
-                    iconBgColor: Color(0xffD3F8DF),
-                    borderColor: Color(0xffAAF0C4),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            MedicalCategories(),
-            SizedBox(height: 12),
-            DoctorsSection(),
-            SizedBox(height: 25),
             MedicalCentersSection(),
           ],
         ),
--- lib/widgets/medical_center_section.dart
+++ lib/widgets/medical_center_section.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assesment/core/assets.dart';
+import 'package:flutter_assesment/widgets/colored_bg.dart';
 
 class MedicalCentersSection extends StatelessWidget {
   const MedicalCentersSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Nearest Medical Center"),
-            TextButton.icon(
-              onPressed: () {},
-              label: Text("See all"),
-              icon: Icon(Icons.arrow_forward_ios),
-              iconAlignment: IconAlignment.end,
-            ),
-          ],
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Nearest Medical Center",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                label: Text(
+                  "See All",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff0A61B4),
+                  ),
+                ),
+                icon: Icon(Icons.arrow_forward_ios, color: Color(0xff0A61B4)),
+                iconAlignment: IconAlignment.end,
+              ),
+            ],
+          ),
         ),
         SizedBox(
-          height: 200,
+          height: 280,
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
+              SizedBox(width: 20),
               ...List.generate(4, (_) {
-                return MedicalCenter();
+                return SizedBox(
+                  width: size.width * .55,
+                  child: MedicalCenter(),
+                );
               }),
             ],
           ),
@@ -49,20 +71,82 @@ class _MedicalCenterState extends State<MedicalCenter> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(4),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+      margin: EdgeInsets.only(right: 15),
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.1),
+            blurRadius: 8,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(child: Image.asset(PNGAssets.hospital)),
-          Text("Tyna Medical Center"),
-          Text("Udomsuk, Bang Na"),
-          Row(
+          Expanded(
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/hospital.png'),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 8,
+                  right: 8,
+                  child: ColoredBg(
+                    padding: EdgeInsets.all(5),
+                    borderRadius: 6,
+                    color: Colors.black.withValues(alpha: .3),
+                    child: Icon(Icons.favorite_border, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("+2k Review"),
+              Text(
+                "Tyna Medical Center",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "Udomsuk, Bang Na",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 14,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 10),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("4,8"),
-                  Icon(Icons.star, color: Colors.amber),
+                  Text("+2k Review"),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("4.8"),
+                      Icon(Icons.star, color: Colors.amber),
+                    ],
+                  ),
                 ],
               ),
             ],
